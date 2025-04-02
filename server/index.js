08b00b0b4061b7edf0d5ac0e92ee965800739f5e
anhkhoa <38072580+anhkhoa2174@users.noreply.github.com>
2025-04-02 10:31:46 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-	console.log(`🚀 Server listening on http://localhost:${PORT}`);
+	console.log(`Server listening on http://localhost:${PORT}`);
 });
 const Room = require("./models/Room");
 
