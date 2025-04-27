08c36960a6dd810cc2ecf2e4440b45ca21ef154c
anhkhoa2174 <tkhoa4040@gmail.com>
2025-04-27 23:01:46 +0700

    added id for schedule

--- server/models/Schedule.js
+++ server/models/Schedule.js
@@ -1,34 +1,42 @@
 const mongoose = require("mongoose");
 
-const scheduleSchema = new mongoose.Schema({
-	room_id: {
-		type: String,
-		required: true,
+const scheduleSchema = new mongoose.Schema(
+	{
+		id: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		room_id: {
+			type: String,
+			required: true,
+		},
+		date: {
+			type: Date,
+			required: true,
+		},
+		usedDate: {
+			type: Date,
+			required: true,
+		},
+		startPeriod: {
+			type: Number,
+			required: true,
+		},
+		endPeriod: {
+			type: Number,
+			required: true,
+		},
+		teacherId: {
+			type: Number,
+			required: true,
+		},
+		lectureTitle: {
+			type: String,
+			required: true,
+		},
 	},
-	date: {
-		type: Date,
-		required: true,
-	},
-	usedDate: {
-		type: Date,
-		required: true,
-	},
-	startPeriod: {
-		type: Number,
-		required: true,
-	},
-	endPeriod: {
-		type: Number,
-		required: true,
-	},
-	teacherId: {
-		type: Number,
-		required: true,
-	},
-	lectureTitle: {
-		type: String,
-		required: true,
-	},
-});
+	{ _id: false }
+);
 
 module.exports = mongoose.model("Schedule", scheduleSchema);
