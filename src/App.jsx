4dafa945488f6b6ffcbadee37cf414ce164fa39b
Tri Dung <ttdung10x@gmail.com>
2025-05-10 16:04:52 +0700

    Add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Login from './pages/Login';
 import RoomInformation from './pages/RoomInformation';
 import ScheduleRegistration from './components/ScheduleRegistration/ScheduleRegistration.jsx';
 import UserInfo from './pages/UserInfo.jsx'
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/setting" element={<UserInfo />} />
-          <Route path='/room/:id' element={<RoomInformation />} />
-          <Route path='/schedule-registration/:id' element={<ScheduleRegistration />} />
+          <Route path="/app/setting" element={<UserInfo />} />
+          <Route path='/app/room/:id' element={<RoomInformation />} />
+          <Route path='/app/schedule-registration/:id' element={<ScheduleRegistration />} />
         </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
