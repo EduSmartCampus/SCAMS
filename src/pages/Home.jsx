fd55fee4dcd20eda35854089dce91173ca376a87
Tri Dung <ttdung10x@gmail.com>
2025-05-21 20:35:06 +0700

    Add ScheduleList

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeaderHome from "../components/Home/HeaderHome";
 import ListRoom from "../components/Home/ListRoom";
+import ScheduleList from "../components/Home/ScheduleList";
 
 const Home = () => {
     return (
         <div>
             <HeaderHome />
             <ListRoom />
+            <ScheduleList />
         </div>
     );
 }
